b221210f98c24df670f690aeebc9b2316267bd5a
Pei Li <37786150+PeiLi-Sandman@users.noreply.github.com>
2018-11-29 11:13:27 -0500

    Create extract pdf.py

---
+++ extract pdf.py
@@ -0,0 +1,16 @@
+#current_directory is the current path for your endnote library, which should be end with PDF, for example: 'G:....\XXX.Data\PDF'
+#new_directory is the new path for all the pdf filess
+
+def get_pdf(current_directory, new_directory):
+    import os
+    from shutil import copyfile
+    deeper_directory = []
+    deepest_directory = []
+    for filename in os.listdir(current_directory):
+        deeper_directory.append(filename)
+    for item in deeper_directory:
+        deepest_directory.append(current_directory+'\\'+item)
+    for item in deepest_directory:
+        for file in os.listdir(item):
+            if file.endswith(".pdf"):
+                copyfile(item+'\\'+file, new_directory+'\\'+file)
