77dfbe2107dd9b8f03058854d9fd432264a73b90
Pei Li <37786150+PeiLi-Sandman@users.noreply.github.com>
2018-12-15 11:55:56 +0800

    add __name__

--- extract pdf.py
+++ extract pdf.py
@@ -2,15 +2,20 @@
 #new_directory is the new path for all the pdf filess
 
 def get_pdf(current_directory, new_directory):
-    import os
-    from shutil import copyfile
-    deeper_directory = []
-    deepest_directory = []
-    for filename in os.listdir(current_directory):
-        deeper_directory.append(filename)
-    for item in deeper_directory:
-        deepest_directory.append(current_directory+'\\'+item)
-    for item in deepest_directory:
-        for file in os.listdir(item):
-            if file.endswith(".pdf"):
-                copyfile(item+'\\'+file, new_directory+'\\'+file)
+	import os
+	from shutil import copyfile
+	deeper_directory = []
+	deepest_directory = []
+	for filename in os.listdir(current_directory):
+		deeper_directory.append(filename)
+	for item in deeper_directory:
+		deepest_directory.append(current_directory+'\\'+item)
+	for item in deepest_directory:
+		for file in os.listdir(item):
+			if file.endswith(".pdf"):
+				copyfile(item+'\\'+file, new_directory+'\\'+file)
+
+if __name__ == "__main__":
+	path1 = input("please input current directory:")
+	path2 = input("pleas input new directory")
+	get_pdf(path1,path2)
